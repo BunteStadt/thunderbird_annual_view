3b0ae6f56100df936ac31ed0b5657f0359d3d6d3
Richard Pergens <richard.pergens@rwth-aachen.de>
2026-07-29 19:35:53 +0200

    fixed function export bug

--- src/ui/year-view/calendar-provider.js
+++ src/ui/year-view/calendar-provider.js
@@ -8,12 +8,17 @@ export class CalendarProvider {
     }
 }
 
-export function resolveCalendarAllDayOnly(calendarId, options = {}) {
+function resolveCalendarAllDayOnly(calendarId, options = {}) {
     const { allDayOnly = false, calendarAllDayModes = {} } = options;
     const mode = calendarAllDayModes?.[calendarId];
     if (mode === "yes") return true;
     if (mode === "no") return false;
     return allDayOnly;
+}
+
+export { resolveCalendarAllDayOnly };
+export function resolveCalendarAllDayOnlyExport(calendarId, options = {}) {
+    return resolveCalendarAllDayOnly(calendarId, options);
 }
 
 export class EmptyCalendarProvider extends CalendarProvider {
